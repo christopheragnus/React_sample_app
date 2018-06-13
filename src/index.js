100bc41bc1bc06b494529bafaa73d2d940d8adbe
Christopher Lam <christopher.lam@students.mq.edu.au>
2018-06-13 19:03:18 +1000

    added helper function

--- src/index.js
+++ src/index.js
@@ -47,12 +47,15 @@ import './index.css';
 
     handleClick(i) {
       const squares = this.state.squares.slice();
-      squares[i] = 'X';
-      this.setState({squares: squares});
+      squares[i] = this.state.xIsNext ? 'X' : '0';
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : '0'); // ternary operator
   
       return (
         <div>
@@ -100,4 +103,22 @@ import './index.css';
     document.getElementById('root')
   );
   
-  
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
